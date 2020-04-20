4faf4371134e68a2037817c1d916750a8c643472
Lovepreet Singh <lovepreet013hundal@gmail.com>
2020-04-20 21:43:38 +0530

    post route added

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
+const Post = require('../models/Post')
+
 
 router.get('/', (req, res) => {
   res.send("WE are at posts")
 })
 
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+})
 
 module.exports = router;
